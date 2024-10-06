f9aab0dfd4263921ea2d2f12883db542bc656f9f
MRB <mehrabshafae@gmail.com>
2024-10-06 09:12:37 +0330

    mrb: mv networks func to cli (test) -> OK

---
+++ network/cli.go
@@ -0,0 +1,444 @@
+package network
+
+import (
+	"math/rand"
+)
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"os"
+	"time"
+
+	// "marboris/core/locales"
+
+	"github.com/gookit/color"
+	"gopkg.in/cheggaaa/pb.v1"
+)
+
+/**
+	derivative
+**/
+
+// Derivative contains the derivatives of `z` and the adjustments
+type Derivative struct {
+	Delta      Matrix
+	Adjustment Matrix
+}
+
+// ComputeLastLayerDerivatives returns the derivatives of the last layer L
+func (network Network) ComputeLastLayerDerivatives() Derivative {
+	l := len(network.Layers) - 1
+	lastLayer := network.Layers[l]
+
+	// Compute derivative for the last layer of weights and biases
+	cost := Difference(network.Output, lastLayer)
+	sigmoidDerivative := Multiplication(lastLayer, ApplyFunction(lastLayer, SubtractsOne))
+
+	// Compute delta and the weights' adjustment
+	delta := Multiplication(
+		ApplyFunction(cost, MultipliesByTwo),
+		sigmoidDerivative,
+	)
+	weights := DotProduct(Transpose(network.Layers[l-1]), delta)
+
+	return Derivative{
+		Delta:      delta,
+		Adjustment: weights,
+	}
+}
+
+// ComputeDerivatives returns the derivatives of a specific layer l defined by i
+func (network Network) ComputeDerivatives(i int, derivatives []Derivative) Derivative {
+	l := len(network.Layers) - 2 - i
+
+	// Compute derivative for the layer of weights and biases
+	delta := Multiplication(
+		DotProduct(
+			derivatives[i].Delta,
+			Transpose(network.Weights[l]),
+		),
+		Multiplication(
+			network.Layers[l],
+			ApplyFunction(network.Layers[l], SubtractsOne),
+		),
+	)
+	weights := DotProduct(Transpose(network.Layers[l-1]), delta)
+
+	return Derivative{
+		Delta:      delta,
+		Adjustment: weights,
+	}
+}
+
+// Adjust make the adjusts
+func (network Network) Adjust(derivatives []Derivative) {
+	for i, derivative := range derivatives {
+		l := len(derivatives) - i
+
+		network.Weights[l-1] = Sum(
+			network.Weights[l-1],
+			ApplyRate(derivative.Adjustment, network.Rate),
+		)
+		network.Biases[l-1] = Sum(
+			network.Biases[l-1],
+			ApplyRate(derivative.Delta, network.Rate),
+		)
+	}
+}
+
+/**
+	math
+**/
+
+// Sigmoid is the activation function
+func Sigmoid(x float64) float64 {
+	return 1 / (1 + math.Exp(-x))
+}
+
+// MultipliesByTwo takes a float and returns the float multiplied by two
+func MultipliesByTwo(x float64) float64 {
+	return 2 * x
+}
+
+// SubtractsOne takes a float and returns the float subtracted by one
+func SubtractsOne(x float64) float64 {
+	return x - 1
+}
+
+/**
+	matrix
+**/
+
+// Matrix is an alias for [][]float64
+type Matrix [][]float64
+
+// RandomMatrix returns the value of a random matrix of *rows* and *columns* dimensions and
+// where the values are between *lower* and *upper*.
+func RandomMatrix(rows, columns int) (matrix Matrix) {
+	matrix = make(Matrix, rows)
+
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]float64, columns)
+		for j := 0; j < columns; j++ {
+			matrix[i][j] = rand.Float64()*2.0 - 1.0
+		}
+	}
+
+	return
+}
+
+// CreateMatrix returns an empty matrix which is the size of rows and columns
+func CreateMatrix(rows, columns int) (matrix Matrix) {
+	matrix = make(Matrix, rows)
+
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]float64, columns)
+	}
+
+	return
+}
+
+// Rows returns number of matrix's rows
+func Rows(matrix Matrix) int {
+	return len(matrix)
+}
+
+// Columns returns number of matrix's columns
+func Columns(matrix Matrix) int {
+	return len(matrix[0])
+}
+
+// ApplyFunctionWithIndex returns a matrix where fn has been applied with the indexes provided
+func ApplyFunctionWithIndex(matrix Matrix, fn func(i, j int, x float64) float64) Matrix {
+	for i := 0; i < Rows(matrix); i++ {
+		for j := 0; j < Columns(matrix); j++ {
+			matrix[i][j] = fn(i, j, matrix[i][j])
+		}
+	}
+
+	return matrix
+}
+
+// ApplyFunction returns a matrix where fn has been applied
+func ApplyFunction(matrix Matrix, fn func(x float64) float64) Matrix {
+	return ApplyFunctionWithIndex(matrix, func(i, j int, x float64) float64 {
+		return fn(x)
+	})
+}
+
+// ApplyRate returns a matrix where the learning rate has been multiplies
+func ApplyRate(matrix Matrix, rate float64) Matrix {
+	return ApplyFunction(matrix, func(x float64) float64 {
+		return rate * x
+	})
+}
+
+// DotProduct returns a matrix which is the result of the dot product between matrix and matrix2
+func DotProduct(matrix, matrix2 Matrix) Matrix {
+	if Columns(matrix) != Rows(matrix2) {
+		panic("Cannot make dot product between these two matrix.")
+	}
+
+	return ApplyFunctionWithIndex(
+		CreateMatrix(Rows(matrix), Columns(matrix2)),
+		func(i, j int, x float64) float64 {
+			var sum float64
+
+			for k := 0; k < Columns(matrix); k++ {
+				sum += matrix[i][k] * matrix2[k][j]
+			}
+
+			return sum
+		},
+	)
+}
+
+// Sum returns the sum of matrix and matrix2
+func Sum(matrix, matrix2 Matrix) (resultMatrix Matrix) {
+	ErrorNotSameSize(matrix, matrix2)
+
+	resultMatrix = CreateMatrix(Rows(matrix), Columns(matrix))
+
+	return ApplyFunctionWithIndex(matrix, func(i, j int, x float64) float64 {
+		return matrix[i][j] + matrix2[i][j]
+	})
+}
+
+// Difference returns the difference between matrix and matrix2
+func Difference(matrix, matrix2 Matrix) (resultMatrix Matrix) {
+	ErrorNotSameSize(matrix, matrix2)
+
+	resultMatrix = CreateMatrix(Rows(matrix), Columns(matrix))
+
+	return ApplyFunctionWithIndex(resultMatrix, func(i, j int, x float64) float64 {
+		return matrix[i][j] - matrix2[i][j]
+	})
+}
+
+// Multiplication returns the multiplication of matrix and matrix2
+func Multiplication(matrix, matrix2 Matrix) (resultMatrix Matrix) {
+	ErrorNotSameSize(matrix, matrix2)
+
+	resultMatrix = CreateMatrix(Rows(matrix), Columns(matrix))
+
+	return ApplyFunctionWithIndex(matrix, func(i, j int, x float64) float64 {
+		return matrix[i][j] * matrix2[i][j]
+	})
+}
+
+// Transpose returns the matrix transposed
+func Transpose(matrix Matrix) (resultMatrix Matrix) {
+	resultMatrix = CreateMatrix(Columns(matrix), Rows(matrix))
+
+	for i := 0; i < Rows(matrix); i++ {
+		for j := 0; j < Columns(matrix); j++ {
+			resultMatrix[j][i] = matrix[i][j]
+		}
+	}
+
+	return resultMatrix
+}
+
+// ErrorNotSameSize panics if the matrices do not have the same dimension
+func ErrorNotSameSize(matrix, matrix2 Matrix) {
+	if Rows(matrix) != Rows(matrix2) && Columns(matrix) != Columns(matrix2) {
+		panic("These two matrices must have the same dimension.")
+	}
+}
+
+/**
+	network
+**/
+
+// Network contains the Layers, Weights, Biases of a neural network then the actual output values
+// and the learning rate.
+type Network struct {
+	Layers  []Matrix
+	Weights []Matrix
+	Biases  []Matrix
+	Output  Matrix
+	Rate    float64
+	Errors  []float64
+	Time    float64
+	Locale  string
+}
+
+// LoadNetwork returns a Network from a specified file
+func LoadNetwork(fileName string) *Network {
+	inF, err := os.Open(fileName)
+	if err != nil {
+		panic("Failed to load " + fileName + ".")
+	}
+	defer inF.Close()
+
+	decoder := json.NewDecoder(inF)
+	neuralNetwork := &Network{}
+	err = decoder.Decode(neuralNetwork)
+	if err != nil {
+		panic(err)
+	}
+
+	return neuralNetwork
+}
+
+// CreateNetwork creates the network by generating the layers, weights and biases
+func CreateNetwork(locale string, rate float64, input, output Matrix, hiddensNodes ...int) Network {
+	input = append([][]float64{
+		make([]float64, len(input[0])),
+	}, input...)
+	output = append([][]float64{
+		make([]float64, len(output[0])),
+	}, output...)
+
+	// Create the layers arrays and add the input values
+	inputMatrix := input
+	layers := []Matrix{inputMatrix}
+	// Generate the hidden layer
+	for _, hiddenNodes := range hiddensNodes {
+		layers = append(layers, CreateMatrix(len(input), hiddenNodes))
+	}
+	// Add the output values to the layers arrays
+	layers = append(layers, output)
+
+	// Generate the weights and biases
+	weightsNumber := len(layers) - 1
+	var weights []Matrix
+	var biases []Matrix
+
+	for i := 0; i < weightsNumber; i++ {
+		rows, columns := Columns(layers[i]), Columns(layers[i+1])
+
+		weights = append(weights, RandomMatrix(rows, columns))
+		biases = append(biases, RandomMatrix(Rows(layers[i]), columns))
+	}
+
+	return Network{
+		Layers:  layers,
+		Weights: weights,
+		Biases:  biases,
+		Output:  output,
+		Rate:    rate,
+		Locale:  locale,
+	}
+}
+
+// Save saves the neural network in a specified file which can be retrieved with LoadNetwork
+func (network Network) Save(fileName string) {
+	outF, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		panic("Failed to save the network to " + fileName + ".")
+	}
+	defer outF.Close()
+
+	encoder := json.NewEncoder(outF)
+	err = encoder.Encode(network)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// FeedForward executes forward propagation for the given inputs in the network
+func (network *Network) FeedForward() {
+	for i := 0; i < len(network.Layers)-1; i++ {
+		layer, weights, biases := network.Layers[i], network.Weights[i], network.Biases[i]
+
+		productMatrix := DotProduct(layer, weights)
+		Sum(productMatrix, biases)
+		ApplyFunction(productMatrix, Sigmoid)
+
+		// Replace the output values
+		network.Layers[i+1] = productMatrix
+	}
+}
+
+// Predict returns the predicted value for a training example
+func (network *Network) Predict(input []float64) []float64 {
+	network.Layers[0] = Matrix{input}
+	network.FeedForward()
+	return network.Layers[len(network.Layers)-1][0]
+}
+
+// FeedBackward executes back propagation to adjust the weights for all the layers
+func (network *Network) FeedBackward() {
+	var derivatives []Derivative
+	derivatives = append(derivatives, network.ComputeLastLayerDerivatives())
+
+	// Compute the derivatives of the hidden layers
+	for i := 0; i < len(network.Layers)-2; i++ {
+		derivatives = append(derivatives, network.ComputeDerivatives(i, derivatives))
+	}
+
+	// Then adjust the weights and biases
+	network.Adjust(derivatives)
+}
+
+// ComputeError returns the average of all the errors after the training
+func (network *Network) ComputeError() float64 {
+	// Feed forward to compute the last layer's values
+	network.FeedForward()
+	lastLayer := network.Layers[len(network.Layers)-1]
+	errors := Difference(network.Output, lastLayer)
+
+	// Make the sum of all the errors
+	var i int
+	var sum float64
+	for _, a := range errors {
+		for _, e := range a {
+			sum += e
+			i++
+		}
+	}
+
+	// Compute the average
+	return sum / float64(i)
+}
+
+// Train trains the neural network with a given number of iterations by executing
+// forward and back propagation
+func (network *Network) Train(iterations int) {
+	// Initialize the start date
+	start := time.Now()
+
+	// Create the progress bar
+	bar := pb.New(iterations).Postfix(fmt.Sprintf(
+		" - %s %s %s",
+		color.FgBlue.Render("Training the"),
+		color.FgRed.Render("english"), // locales.GetNameByTag(network.Locale)
+		color.FgBlue.Render("neural network"),
+	))
+	bar.Format("(██░)")
+	bar.SetMaxWidth(60)
+	bar.ShowCounters = false
+	bar.Start()
+
+	// Train the network
+	for i := 0; i < iterations; i++ {
+		network.FeedForward()
+		network.FeedBackward()
+
+		// Append errors for dashboard data
+		if i%(iterations/20) == 0 {
+			network.Errors = append(
+				network.Errors,
+				// Round the error to two decimals
+				network.ComputeError(),
+			)
+		}
+
+		// Increment the progress bar
+		bar.Increment()
+	}
+
+	bar.Finish()
+	// Print the error
+	arrangedError := fmt.Sprintf("%.5f", network.ComputeError())
+
+	// Calculate elapsed date
+	elapsed := time.Since(start)
+	// Round the elapsed date at two decimals
+	network.Time = math.Floor(elapsed.Seconds()*100) / 100
+
+	fmt.Printf("The error rate is %s.\n", color.FgGreen.Render(arrangedError))
+}
