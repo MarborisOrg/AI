adbd007d811036008f7be4b4672b772b6f73ece9
MRB <mehrabshafae@gmail.com>
2024-10-09 21:52:52 +0330

    mrb: cu (Test -> OK)

--- main.go
+++ main.go
@@ -5,27 +5,25 @@ import (
 	"fmt"
 
 	"github.com/gookit/color"
-
-	"marboris/core/cli"
 )
 
-var neuralNetworks = map[string]cli.Network{}
+var neuralNetworksMain = map[string]Network{}
 
 func main() {
 	port := flag.String("port", "8080", "The port for the API and WebSocket.")
 	flag.Parse()
 
-	marborisASCII := string(cli.ReadFile("res/marboris-ascii.txt"))
+	marborisASCII := string(ReadFile("res/marboris-ascii.txt"))
 	fmt.Println(color.FgLightGreen.Render(marborisASCII))
 
-	cli.Authenticate()
+	Authenticate()
 
 	// en
-	for _, locale := range cli.Locales {
-		cli.SerializeMessages(locale.Tag)
+	for _, locale := range Locales {
+		SerializeMessages(locale.Tag)
 
-		neuralNetworks[locale.Tag] = cli.CreateNeuralNetwork(locale.Tag)
+		neuralNetworksMain[locale.Tag] = CreateNeuralNetwork(locale.Tag)
 	}
 
-	cli.Serve(neuralNetworks, *port)
+	Serve(neuralNetworksMain, *port)
 }
